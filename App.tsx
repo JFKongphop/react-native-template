6e3e7def5d82a50a2e7535f1cfded63a48d9493b
JFKongphop <93483308+JFKongphop@users.noreply.github.com>
2024-06-11 22:55:02 +0700

    create import alias

--- App.tsx
+++ App.tsx
@@ -1,31 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { type NavigationProp } from '@react-navigation/native';
+import App1 from '@/screens/App1';
 
 type RootStackParamList = {
   App1: undefined;
   App2: undefined;
 };
 
+
+export type Navigation = NavigationProp<RootStackParamList>;
+
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {/* <Stack.Screen 
-          name='App1'
-          component={App1}
-        />
-        <Stack.Screen 
-          name='App2'
-          component={App2}
-        /> */}
-      </Stack.Navigator>
-    </NavigationContainer>
+
+    <SafeAreaView>
+<App1 />
+    </SafeAreaView>
+    
+
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     {/* <Stack.Screen 
+    //       name='App1'
+    //       component={App1}
+    //     />
+    //     <Stack.Screen 
+    //       name='App2'
+    //       component={App2}
+    //     /> */}
+    //   </Stack.Navigator>
+    // </NavigationContainer>
   );
 }
 
